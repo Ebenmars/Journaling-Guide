2585f3f45cd6ed568e8479fb65a279eb6dade9e9
Ebenmars <76241509+Ebenmars@users.noreply.github.com>
2023-12-29 20:29:09 -0500

    Added Model and  Cleaned Up HTML & CSS

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 "use strict";
 
+const model = document.querySelector(".model");
+const overlay = document.querySelector(".overlay");
+const btnCloseModel = document.querySelector(".close-model");
+const btnsOpenModel = document.querySelectorAll(".show-model");
+console.log(btnsOpenModel);
+
+for(let i = 0; i < btnsOpenModel.length; i++){
+    console.log(btnsOpenModel[i].textContent);
+    btnsOpenModel[i].addEventListener("click",function(){
+        console.log("Button clicked");
+        model.classList.remove('hidden');
+        overlay.classList.remove("hidden");
+    });
+}
